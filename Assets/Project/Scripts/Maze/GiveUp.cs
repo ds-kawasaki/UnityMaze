6b29b7305b8f5b0ab898209954e2d91a9fa47ef4
川﨑　澄明 <kawasaki@d-seeds.com>
2021-10-07 15:41:40 +0900

    GiveUpボタン追加

---
+++ Assets/Project/Scripts/Maze/GiveUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ClickGiveUp()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("scnTitle");
+    }
+}
